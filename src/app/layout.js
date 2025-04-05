eb6b388cac7d9a2e5cad8da29e36fc6cf42074fe
vinayakconstructions <vinayakconstructions101@gmail.com>
2025-04-05 16:37:42 +0530

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="icon" href='/image.svg' />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
       <Header />
         {children}
